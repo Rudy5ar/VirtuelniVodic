64841ba496180f02836cc02eb615652594f935fd
Petar Rudovic <petar.rudovic2@gmail.com>
2024-04-10 22:40:48 +0200

    Initial commit

---
+++ VirtuelniVodic/src/main/java/com/pmf/pris/VirtuelniVodicApplication.java
@@ -0,0 +1,13 @@
+package com.pmf.pris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtuelniVodicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtuelniVodicApplication.class, args);
+	}
+
+}
---
+++ VirtuelniVodic/src/test/java/com/pmf/pris/VirtuelniVodicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmf.pris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtuelniVodicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
