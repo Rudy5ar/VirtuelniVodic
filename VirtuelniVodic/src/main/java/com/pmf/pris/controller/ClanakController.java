b2c3446a23dbb88e1f353709355c0cb836d4a0cc
ackeza <166657641+ackeza@users.noreply.github.com>
2024-05-21 21:52:51 +0200

    Dodao controller Metoda

--- VirtuelniVodic/src/main/java/com/pmf/pris/controller/ClanakController.java
+++ VirtuelniVodic/src/main/java/com/pmf/pris/controller/ClanakController.java
@@ -1,10 +1,26 @@
 package com.pmf.pris.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("clanak")
 public class ClanakController {
-
+	@PostMapping("kreirajTuru")
+	public String kreirajTuru(HttpServletRequest request, @RequestParam("naziv") String naziv, @RequestParam("opis") String opis) {
+		
+		// Promeniti 1 u request.getAttribute(idKorisnika) kada security bude implementiran
+		if(ts.kreirajTuru(naziv, opis, 1) == false) {
+			request.setAttribute("uspelo", "Nije kreirana tura");
+			System.out.println();
+			return "ture/turaNijeSacuvana";
+		}
+		request.setAttribute("uspelo", "Kreirana tura");
+		return "ture/prikaziSacuvanuTuru";
+	}
 }
+
