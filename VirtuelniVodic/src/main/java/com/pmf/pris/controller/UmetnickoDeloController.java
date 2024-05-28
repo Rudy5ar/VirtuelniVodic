c98be8b8c29f18ed85f9b68c4c9c8de1ae8018f4
ackeza <obrenovic.aleksandar@yahoo.com>
2024-05-28 14:05:20 +0200

    Ispravljeno dodavanje/kreiranje umetnickihDela

--- VirtuelniVodic/src/main/java/com/pmf/pris/controller/UmetnickoDeloController.java
+++ VirtuelniVodic/src/main/java/com/pmf/pris/controller/UmetnickoDeloController.java
@@ -1,12 +1,16 @@
 package com.pmf.pris.controller;
 
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import com.pmf.pris.service.UmetnickoDeloService;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -28,25 +32,20 @@ public class UmetnickoDeloController {
         return "home";
     }
     
-    @PostMapping("dodajUmetnickoDelo")
-    public String dodajUmetnickoDelo(HttpServletRequest request, @RequestParam("naziv") String naziv, @RequestParam("opis") String opis) {
-        if(uds.dodajUmetnickoDelo(naziv, opis)) {
-            request.setAttribute("uspelo", "Umetničko delo dodato");
-            return "pregledPredmeta";
-        } else {
-            request.setAttribute("uspelo", "Nije dodato umetničko delo");
-            return "greska";
-        }
-    }
-    
-    @PostMapping("promeniOpisUmetnickogDela")
-    public String promeniOpisUmetnickogDela(HttpServletRequest request, @RequestParam("idDela") int idDela, @RequestParam("noviOpis") String noviOpis) {
-        if(uds.promeniOpisUmetnickogDela(idDela, noviOpis)) {
-            request.setAttribute("uspelo", "Opis umetničkog dela promenjen");
-            return "pregledPredmeta";
-        } else {
-            request.setAttribute("uspelo", "Nije promenjen opis umetničkog dela");
-            return "greska";
-        }
-    }
+	@PostMapping("kreirajUmetnickoDelo")
+	public String kreirajTuru(HttpServletRequest request, @RequestParam("naziv") String naziv, 
+			@RequestParam("opis") String opis, @RequestParam("datum") Date datum,
+			@RequestParam("geografskaDuzina") double geografskaDuzina, 	@RequestParam("geografskaSirina") double geografskaSirina) {
+		
+		
+		// Promeniti 1 u request.getAttribute(idKorisnika) kada security bude implementiran
+		if(uds.kreirajUmetnickoDelo(naziv, opis, datum, geografskaDuzina, geografskaSirina, 1) == false) {
+			request.setAttribute("uspelo", "Nije kreirano umetnicko delo");
+			System.out.println();
+			return "umetnickoDeloNijeSacuvano";
+		}
+		request.setAttribute("uspelo", "Kreirano umetnicko delo");
+		return "prikaziSacuvanoUmetnickoDelo";
+	}
 }
+
--- VirtuelniVodic/src/main/java/com/pmf/pris/service/UmetnickoDeloService.java
+++ VirtuelniVodic/src/main/java/com/pmf/pris/service/UmetnickoDeloService.java
@@ -1,11 +1,15 @@
 package com.pmf.pris.service;
 
+import java.util.Date;
 import java.util.List;
-import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.pmf.pris.repository.TuraRepository;
 import com.pmf.pris.repository.UmetnickoDeloRepository;
+import com.pmf.pris.repository.UmetnikRepository;
+
 import model.Tura;
 import model.Umetnickodelo;
 
@@ -18,38 +22,35 @@ public class UmetnickoDeloService {
     @Autowired
     TuraRepository tr;
     
+    @Autowired
+    UmetnikRepository ur;
+    
     public List<Umetnickodelo> getDelaUTuri(int idTure) {
         Tura t = tr.findById(idTure).get();
-        List<Umetnickodelo> delaUTuri = t.getUmetnickodelos();
-        return delaUTuri;
+        return t.getUmetnickodelos();
     }
     
-    public List<Umetnickodelo> getDela(){
+    public List<Umetnickodelo> getDela() {
         return udr.findAll();
     }
     
-    public boolean dodajUmetnickoDelo(String naziv, String opis) {
-        try {
-            Umetnickodelo novoDelo = new Umetnickodelo();
-            novoDelo.setNaziv(naziv);
-            novoDelo.setOpis(opis);
-            udr.save(novoDelo);
-            return true;
-        } catch (Exception e) {
-            System.out.println("Nije dodato umetničko delo: " + e.getMessage());
-            return false;
-        }
-    }
-    
-    public boolean promeniOpisUmetnickogDela(int idDela, String noviOpis) {
-        Optional<Umetnickodelo> optionalDelo = udr.findById(idDela);
-        if (optionalDelo.isPresent()) {
-            Umetnickodelo umetnickoDelo = optionalDelo.get();
-            umetnickoDelo.setOpis(noviOpis);
-            udr.save(umetnickoDelo);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public boolean kreirajUmetnickoDelo(String naziv, String opis, Date datum,
+			double geografskaDuzina, double geografskaSirina, int i) {
+		Umetnickodelo novaUmetnickoDelo = new Umetnickodelo();
+		novaUmetnickoDelo.setNaziv(naziv);
+		novaUmetnickoDelo.setOpis(opis);
+		novaUmetnickoDelo.setDatum(datum);
+		novaUmetnickoDelo.setGeografskaDuzina(geografskaDuzina);
+		novaUmetnickoDelo.setGeografskaSirina(geografskaSirina);
+		novaUmetnickoDelo.setUmetnik(ur.findById(i).get());
+		
+		try {
+			udr.save(novaUmetnickoDelo);
+		} catch (Exception e) {
+			System.out.println("Nije dobro sacuvano");
+		    return false;
+		}
+
+		return true;
+	}
 }
